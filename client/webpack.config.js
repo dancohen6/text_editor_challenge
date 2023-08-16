3b9c4b0049499e5fce7c1c2c3c94cbf0a69b61cd
dancohen6 <dzcsix@gmail.com>
2023-08-16 13:26:28 -0400

    webpack config added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,3 +5,76 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+
+      new WebpackPwaManifest({
+        name: 'CodePad',
+        short_name: 'CodePad',
+        description: 'A text editor for web developers',
+        background_color: '#ffffff',
+        theme_color: '#fff',
+        start_url: '.',
+        publicPath: '/',
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [48, 96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+
+    devServer: {
+      compress: true,
+      hot: true,
+      watchFiles: ['./src/index.html'],
+    },
+  };
+};
+
